2f2e8ab05122e3ac9e5c416f6304a98f4ffdf084
Will Szumski <will@stackhpc.com>
2019-09-10 10:20:38 +0100

    Always run all tests

--- .travis/test.sh
+++ .travis/test.sh
@@ -2,16 +2,4 @@
 
 set -euo pipefail
 
-if [ ! -d "./molecule/latest" ]; then	
-	tox -- molecule test --all
-	exit 0
-fi
-
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-	tox -- molecule test --all --destroy always
-else
-	tox -- molecule test -s default --destroy always
-	if [ -d "./molecule/alternative" ]; then
-		tox -- molecule test -s alternative --destroy never
-	fi
-fi
+tox -- molecule test --all
