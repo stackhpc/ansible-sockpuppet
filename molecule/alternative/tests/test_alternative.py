f933dd6c45cb5875951da8a714c04d7db2f72410
Will Szumski <will@stackhpc.com>
2019-09-11 16:18:51 +0100

    Fix lint warnings
    /home/travis/build/stackhpc/ansible-sockpuppet/molecule/alternative/tests/test_alternative.py:1:1: F401 'pytest' imported but unused
    /home/travis/build/stackhpc/ansible-sockpuppet/molecule/alternative/tests/test_alternative.py:8:1: E302 expected 2 blank lines, found 1

--- molecule/alternative/tests/test_alternative.py
+++ molecule/alternative/tests/test_alternative.py
@@ -1,9 +1,9 @@
-import pytest
 import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
 
 def test_directories(host):
     dirs = [
